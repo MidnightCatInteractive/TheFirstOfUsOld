f3cbd5083b59f3b582b522f7d2b0fa76caa59d61
David <60017346+DavidMaglica@users.noreply.github.com>
2023-05-08 23:44:17 +0200

    Settings Menu improvement
    Added animations and scripts, each setting now actually does something
    TODO: animations on volume sliders

--- Assets/Scripts/MenuScripts/SettingsMenuScript.cs
+++ Assets/Scripts/MenuScripts/SettingsMenuScript.cs
@@ -2,10 +2,76 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
 
-
+[RequireComponent(typeof(AudioMixer))]
+[RequireComponent(typeof(Dropdown))]
 public class SettingsMenuScript : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+    public TMP_Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        int currentResolutionIndex = 0;
+        int index = 0;
+
+        List<string> resolutionList = new List<string>();
+        foreach (Resolution resolution in resolutions)
+        {
+            string option = resolution.width + " x " + resolution.height;
+            resolutionList.Add(option);
+
+            if (resolution.width == Screen.currentResolution.width && resolution.height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = index;
+            }
+            index++;
+        }
+
+        resolutionDropdown.AddOptions(resolutionList);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetFullScreen (bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetMasterVolume (float volume)
+    {
+        audioMixer.SetFloat("MasterVolume", volume);
+    }
+
+    public void SetSFxVolume (float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", volume);
+    }
+
+    public void SetMusicVolume (float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+
     public void GoToMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
